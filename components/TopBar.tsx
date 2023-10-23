b7ccc577d5d63c15d5ba998bf606a627c1e76c84
Berl Treasure Campomanes <berlcamp@gmail.com>
2023-10-23 12:58:30 +0800

    ..

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -13,7 +13,7 @@ function TopBar () {
     <div className='fixed top-0 right-0 z-20 p-2 flex items-center w-full bg-gray-50 shadow-md'>
       <div className='flex-1'>
         <div className='ml-14 lg:ml-64'>
-          <span className='ml-2 border border-gray-500 py-px px-1 font-medium text-gray-800 text-sm'>{process.env.NEXT_PUBLIC_ORG_NAME}</span>
+          {/* <span className='ml-2 border border-gray-500 py-px px-1 font-medium text-gray-800 text-sm'>{process.env.NEXT_PUBLIC_ORG_NAME}</span> */}
         </div>
       </div>
       <div className='flex space-x-2'>
